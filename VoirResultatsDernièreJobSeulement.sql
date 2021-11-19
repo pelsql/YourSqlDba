fd7d68c732e67a17f02c5aa15b23800764fa02c6
Maurice Pelchat <maurice.pelchat@outlook.com>
2021-11-18 20:31:02 -0500

    6.7.3.0 Other ++ to readme and other file cleanup

--- VoirResultatsDernièreJobSeulement.sql
+++
@@ -1,4 +0,0 @@
-declare @jobno int; select top 1 @jobno = jobno from maint.JobHistory order by jobno desc
-delete  from maint.JobHistory where jobno < @jobno
-exec yoursqldba.maint.Showhistory
-exec yoursqldba.maint.ShowHistoryErrors 
